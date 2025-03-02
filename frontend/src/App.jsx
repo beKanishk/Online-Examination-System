8bacec5d5aa7a5edf24707a18ceb998a9616ae6a
a-rya13 <aryaagarwal20031@gmail.com>
2025-03-02 19:37:17 +0530

    changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,23 @@ import {
 } from "react-router-dom";
 
 import User from "./components/User.jsx";
+import Exams from "./components/Exams.jsx";
 
 function App() {
- 
-
   return (
     <BrowserRouter>
+      <nav>
+        <ul>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/exams">Exams</Link></li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/login" element={<User />} />
-        </Routes>
+        <Route path="/exams" element={<Exams />} />
+      </Routes>
     </BrowserRouter>
   );
- 
 }
 
-export default App
-
+export default App;
