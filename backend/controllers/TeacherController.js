ef436d88721f1f3d9c13b7104566c1ab45cc39be
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2025-03-03 23:57:04 +0530

    login controller added in teacher

--- backend/controllers/TeacherController.js
+++ backend/controllers/TeacherController.js
@@ -13,7 +13,7 @@ export const registerTeacher = async (req, res) => {
       return res.status(400).json({ message: "Teacher already exists" });
 
     const hashedpass = bcrypt.hashSync(password, salt);
-    const newTeacher =  new Teacher({
+    const newTeacher = new Teacher({
       name,
       email,
       password: hashedpass,
@@ -23,8 +23,35 @@ export const registerTeacher = async (req, res) => {
       role: "Teacher",
     });
     await newTeacher.save();
-    res.status(201).json({ message: "Teacher registered successfully" ,newTeacher});
+    res
+      .status(201)
+      .json({ message: "Teacher registered successfully", newTeacher });
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email && !password) {
+    res.status(422).json({ message: "fileds cannot be empty" });
+  }
+  let teacher;
+  try {
+    teacher = await Teacher.findOne({ email });
+    if (!teacher) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  const comparePass = bcrypt.compareSync(password, user.password);
+  const success = false;
+  if (!comparePass) {
+    res.status(400).json({ success, message: "invalid password" });
+  } else {
+    res
+      .status(200)
+      .json({ success: "true", message: "login successfull", teacher });
   }
 };
