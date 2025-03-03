5de813cfef17bd73da47bfb1189b6d454597dfbe
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2025-03-03 23:51:26 +0530

    teacher register controller created

--- backend/controllers/TeacherController.js
+++ backend/controllers/TeacherController.js
@@ -1,7 +1,30 @@
 import bcrypt from "bcryptjs";
-import jwt from 'jsonwebtoken';
+
 import Teacher from "../models/Teacher";
 
-export const registerTeacher=async(req,res)=>{
-    const{nam,email,password,department,}=req.body;
-}
+const salt = bcryptjs.genSaltSync(10);
+export const registerTeacher = async (req, res) => {
+  const { name, email, password, department, coursesTaught, profilePicture } =
+    req.body;
+
+  try {
+    const existingTeacher = await Teacher.findOne({ email });
+    if (existingTeacher)
+      return res.status(400).json({ message: "Teacher already exists" });
+
+    const hashedpass = bcrypt.hashSync(password, salt);
+    const newTeacher =  new Teacher({
+      name,
+      email,
+      password: hashedpass,
+      department,
+      profilePicture,
+      coursesTaught,
+      role: "Teacher",
+    });
+    await newTeacher.save();
+    res.status(201).json({ message: "Teacher registered successfully" ,newTeacher});
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
